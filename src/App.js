2b75cad55ae6d407fe8a26ef9e7b1fb3d5b16710
Muhammad Abdullah <mabdullah@ogoul.com>
2024-03-08 19:29:49 +0500

    updated

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="desktop-3">
+        <div className="desktop-3-child">
+        </div>
+        <div className="desktop-3-item">
+        </div>
+        <div className="desktop-3-inner">
+        </div>
+
+        <div className="nav">
+          <div className="divmega-nav-center-wrapper">
+            <div className="divmega-nav-right-buttons-wr">
+              <div className="link-login">Login</div>
+              <div className="link">
+                <div className="divbtn-overflow">
+                  <b className="get-started">Get Started</b>
+                </div>
+              </div>
+            </div>
+            <img className="untitled-1-2-icon" alt="" src="/assets/a1.png" />
+          </div>
+        </div>
+        <div className="heading-1-run-scale-your-parent">
+          <b className="heading-1">Run & scale your</b>
+          <b className="heading-1-container">
+            <span className="heading-1-container1">
+              <span>go-to-market — </span>
+              <span className="with-ai">with AI</span>
+            </span>
+          </b>
+          <div className="create-content-enrich-container">
+            <span className="heading-1-container1">
+              <p className="create-content-enrich">Create content, enrich your CRM, scale your prospecting efforts,
+                and much more. Our AI platform is designed for your</p>
+              <p className="create-content-enrich">entire GTM team. Install our powerful, pre-built workflows — or
+                build your own in seconds.</p>
+            </span>
+          </div>
+          <div className="form-formref-input-parent">
+            <div className="form-formref-input">
+              <div className="divplaceholder">
+                <div className="whats-your-email">What's your email?</div>
+              </div>
+            </div>
+            <div className="form-formref-input1">
+              <div className="continue">Continue</div>
+            </div>
+          </div>
+          <div className="by-clicking-continue-container">
+            <span className="heading-1-container1">
+              <span>By clicking "Continue" you agree to AInytime’s </span>
+              <span className="privacy-notice">Privacy Notice</span>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
